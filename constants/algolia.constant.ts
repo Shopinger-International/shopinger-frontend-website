9eccc52115e001230f80d0f1fca710ccfa985826
= <=>
2026-07-29 12:37:40 +0530

    [main] updated index name for production

--- constants/algolia.constant.ts
+++ constants/algolia.constant.ts
@@ -1,4 +1,5 @@
+const IS_PROD = process.env.NODE_ENV === "production";
 export const ALGOLIA_INDEX = {
-  PRODUCTS: "products",
-  QUERIES: "product_queries",
+  PRODUCTS: IS_PROD ? "prod_products" : "products",
+  QUERIES: IS_PROD ? "prod_product_queries" : "product_queries",
 };
