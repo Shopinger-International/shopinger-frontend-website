0b6a9582a8c5d47dd0ef6abac513c05519014dc7
= <=>
2026-08-03 17:28:52 +0530

    [main] fixes

--- constants/algolia.constant.ts
+++ constants/algolia.constant.ts
@@ -1,7 +1,7 @@
 const IS_PROD = process.env.NODE_ENV === "production";
-console.log("value of prod",IS_PROD);
+console.log("value of prod", IS_PROD);
 
 export const ALGOLIA_INDEX = {
-  PRODUCTS: IS_PROD ? "prod_products" : "products",
-  QUERIES: IS_PROD ? "prod_product_queries" : "product_queries",
+  PRODUCTS: IS_PROD ? "prod_products" : "stage_products",
+  QUERIES: IS_PROD ? "prod_product_queries" : "stage_product_queries",
 };
