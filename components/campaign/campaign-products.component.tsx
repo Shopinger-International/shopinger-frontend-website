e3aa46e051259708fa6949500b61d6222e1ab30e
priya-shopinger <priya@shopinger.co.in>
2026-09-02 12:50:49 +0530

    Fix campaign products mobile grid

--- components/campaign/campaign-products.component.tsx
+++ components/campaign/campaign-products.component.tsx
@@ -126,7 +126,7 @@ const CampaignProducts: FC<IProps> = ({ campaign_id }) => {
   }, [hasNextPage, isProductPending]);
   return (
     <>
-      <div className="grid grid-cols-1 gap-4 px-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-cols-2 gap-4 px-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
         {isProductPending
           ? Array.from({ length: 12 }).map((_, i) => (
               <ProductCardSkeleton key={`initial-skeleton-${i}`} />
