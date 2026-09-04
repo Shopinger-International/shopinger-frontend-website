f576fa45faf241ccbebbea9bbbe3bf37237a85db
priya-shopinger <priya@shopinger.co.in>
2026-09-04 12:46:12 +0530

    feat(login): update responsive login modal

--- components/login/login-modal.component.tsx
+++ components/login/login-modal.component.tsx
@@ -1,13 +1,9 @@
-// types
 import type { FC } from "react";
 import type IUser from "@/types/user";
-// icons
 import { X } from "lucide-react";
-
-// external components
 import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 
-// local components
+import LoginInfoSection from "@/components/login/login-info-section.component";
 import LoginForm from "@/components/login/login-form.component";
 
 type IProps = {
@@ -19,28 +15,38 @@ type IProps = {
 const LoginModal: FC<IProps> = ({ open, handleClose, handleOnSuccess }) => {
   return (
     <Dialog open={open} onClose={handleClose} className="relative z-50">
-      <DialogBackdrop className="fixed inset-0 bg-black/40 backdrop-blur-xs" />
+    <DialogBackdrop className="fixed inset-0 bg-black/20 backdrop-blur-xs lg:bg-[#FFE2D0]" />
 
-      {/* container */}
       <div className="fixed inset-0 flex items-end justify-center lg:items-center">
-        <DialogPanel className="relative max-h-[90vh] w-full overflow-hidden rounded-t-2xl bg-white shadow-xl lg:h-auto lg:max-h-[95vh] lg:w-105 lg:rounded-2xl">
+        <DialogPanel className="relative max-h-[95vh] w-full overflow-hidden rounded-none bg-white shadow-xl lg:w-max">
+          {/* Close Button */}
           <button
-            className="absolute top-4 right-4 z-2 cursor-pointer text-gray-600"
+            className="absolute top-3 right-3 z-50 flex size-8 items-center justify-center text-gray-600 hover:text-gray-900"
             onClick={handleClose}
+            aria-label="Close login"
           >
-            <X />
+            <X className="size-6" />
           </button>
 
-          <LoginForm
-            is_modal={true}
-            heading_text="Login to complete your order"
-            handleOnSuccess={(user) => {
-              handleOnSuccess(user);
-            }}
-          />
+          {/* Desktop Login */}
+          <div className="hidden lg:flex">
+            <LoginInfoSection />
+
+            <LoginForm handleOnSuccess={handleOnSuccess} />
+          </div>
+
+          {/* Mobile Login */}
+          <div className="lg:hidden">
+            <LoginForm
+              is_modal={true}
+              heading_text="Login to complete your order"
+              handleOnSuccess={handleOnSuccess}
+            />
+          </div>
         </DialogPanel>
       </div>
     </Dialog>
   );
 };
+
 export default LoginModal;
