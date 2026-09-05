413b22bafa27079d3ef4fd1ed29f1f9bcdda1f49
Ankit-0018 <as1777766@gmail.com>
2026-09-05 17:16:26 +0530

    remove imports

--- components/header/header.component.tsx
+++ components/header/header.component.tsx
@@ -12,7 +12,7 @@ import AccountDropdown from "@/components/header/account-dropdown.component";
 import FilterSortBar from "@/components/categories/filter-sort-bar.component";
 
 // icons
-import { Menu, ChevronRight, CircleUserIcon, MapPin, ZapIcon } from "lucide-react";
+import { Menu, ChevronRight, CircleUserIcon, MapPin } from "lucide-react";
 
 // helpers
 import { clsx } from "clsx";
@@ -72,7 +72,7 @@ const LocationBlock: FC<{
             </span>
             <div className="flex flex-row items-center gap-1">
               <span className="flex gap-1 rounded-md bg-[#FF6900] px-2 py-1 text-[10px] font-bold whitespace-nowrap text-white">
-                {/* <ZapIcon className="size-3" fill="white" /> */} {delivery_time} MIN 
+               {delivery_time} MIN 
               </span>
             </div>
           </div>
@@ -91,7 +91,6 @@ const LocationBlock: FC<{
             )}
           >
             <span className="flex items-center gap-1">
-              {/* <ZapIcon size={14} fill="white" /> */}
               {delivery_time} MIN
             </span>
           </span>
