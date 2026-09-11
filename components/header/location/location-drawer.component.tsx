ff988ad449b42ccf1b33303c61225fe9aed195a6
Ashish <ashish_classic@proton.me>
2026-09-11 17:50:35 +0530

    [feat/current-location-picker] fixes

--- components/header/location/location-drawer.component.tsx
+++ components/header/location/location-drawer.component.tsx
@@ -29,7 +29,7 @@ const LocationDrawer: FC<IProps> = ({ toggle, open, onClose }) => {
       <div className="fixed inset-0 flex items-end justify-center">
         <DialogPanel
           transition
-          className="relative h-2/3 w-full transform overflow-visible rounded-t-xl bg-white pb-[env(safe-area-inset-bottom)] shadow-[0_-8px_30px_rgba(0,0,0,0.12)] transition-transform duration-300 ease-out outline-none data-closed:translate-y-full border-2 border-red-900"
+          className="relative h-2/3 w-full transform overflow-visible rounded-t-xl bg-white pb-[env(safe-area-inset-bottom)] shadow-[0_-8px_30px_rgba(0,0,0,0.12)] transition-transform duration-300 ease-out outline-none data-closed:translate-y-full"
         >
           {/* Keep floating close CTA */}
           {toggle && (
@@ -44,7 +44,7 @@ const LocationDrawer: FC<IProps> = ({ toggle, open, onClose }) => {
           )}
 
           {/* Content */}
-          <div className="overflow-y-auto border border-green-800 h-full">
+          <div className="h-full overflow-y-auto">
             <LocationTooltipContent handleClose={onClose} />
           </div>
         </DialogPanel>
