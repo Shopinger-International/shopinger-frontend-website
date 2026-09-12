36a835b25075d9ac656dca642b6ff738fbffe953
Ashish <ashish_classic@proton.me>
2026-09-12 18:14:28 +0530

    [feat/current-location-picker] fixes

--- components/header/location/location-drawer.component.tsx
+++ components/header/location/location-drawer.component.tsx
@@ -31,18 +31,6 @@ const LocationDrawer: FC<IProps> = ({ toggle, open, onClose }) => {
           transition
           className="relative h-full w-full transform overflow-hidden bg-white pb-[env(safe-area-inset-bottom)] shadow-[0_-8px_30px_rgba(0,0,0,0.12)] transition-transform duration-300 ease-out outline-none data-closed:translate-y-full"
         >
-          {/* Keep floating close CTA */}
-          {toggle && (
-            <button
-              type="button"
-              onClick={() => toggle && onClose()}
-              aria-label="Close"
-              className="absolute top-0 left-1/2 z-20 flex size-12 -translate-x-1/2 -translate-y-[calc(100%+20px)] items-center justify-center rounded-full bg-black text-white shadow-lg transition hover:bg-gray-900 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-offset-2 focus-visible:ring-offset-black"
-            >
-              <X className="size-6" strokeWidth={2.5} />
-            </button>
-          )}
-
           {/* Content */}
           <div className="h-full overflow-y-auto">
             <LocationTooltipContent toggle={toggle} handleClose={onClose} />
