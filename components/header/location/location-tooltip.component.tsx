d3a2c26c376741dca364fefc610b689dbbdeb4ab
Ashish <ashish_classic@proton.me>
2026-09-19 13:34:16 +0530

    [feat/mobile-header] added mobile header

--- components/header/location/location-tooltip.component.tsx
+++ components/header/location/location-tooltip.component.tsx
@@ -36,7 +36,7 @@ const LocationTooltip: FC<{
 
   if (is_mobile) {
     return (
-      <div className={clsx("min-w-0 items-center text-white", className)}>
+      <div className={clsx("min-w-0 items-center", className)}>
         {/* ================= MOBILE ================= */}
         <div className="flex w-full min-w-0 items-center gap-2 lg:hidden">
           {/* Location */}
@@ -44,9 +44,9 @@ const LocationTooltip: FC<{
             onClick={() => updateLocationDrawerState?.(true)}
             className="flex min-w-0 flex-1 items-center gap-1"
           >
-            <MapPin aria-hidden={true} className="size-3 shrink-0 text-white" />
+            <MapPin aria-hidden={true} className="size-4 shrink-0 stroke-3" />
 
-            <span className="min-w-0 truncate text-sm">
+            <span className="min-w-0 truncate text-sm font-medium">
               {selected_address ?? "Choose delivery location"}
             </span>
             {/* Arrow */}
@@ -54,7 +54,7 @@ const LocationTooltip: FC<{
           </button>
 
           {/* Delivery */}
-          <div className="flex shrink-0 items-center gap-1">
+          {/* <div className="flex shrink-0 items-center gap-1">
             <div>
               <span className="text-[11px] font-semibold whitespace-nowrap">
                 Delivery in
@@ -65,7 +65,7 @@ const LocationTooltip: FC<{
                 </span>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
 
         {/* ================= DESKTOP ================= */}
---
+++ components/header/mobile-header.component.tsx
@@ -0,0 +1,119 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+// types
+import type { FC, ReactNode } from "react";
+
+// icons
+import { CircleUserIcon } from "lucide-react";
+import GroceryIcon from "@/components/common/icons/grocery.icon";
+import PharmacyIcon from "@/components/common/icons/pharmacy.icon";
+import ShopingerIcon from "@/components/common/icons/shopinger.icon";
+
+// local components
+import LocationTooltip from "@/components/header/location/location-tooltip.component";
+import SearchBar from "@/components/header/search-bar/search-bar.component";
+
+// hooks
+import useIsMounted from "@/hooks/common/use-is-mounted.hook";
+
+// helpers
+import { cn } from "@/lib/utils";
+import clsx from "clsx";
+
+type ICategory = {
+  label: string;
+  href: string;
+  icon: ReactNode;
+};
+
+const categories: ICategory[] = [
+  {
+    label: "Shopinger",
+    href: "/",
+    icon: <ShopingerIcon size={100} />,
+  },
+  {
+    label: "Grocery",
+    href: "/categories/Grocery",
+    icon: <GroceryIcon size={28} />,
+  },
+  {
+    label: "Pharmacy",
+    href: "/categories/Health-and-Personal-Care",
+    icon: <PharmacyIcon size={28} />,
+  },
+];
+
+const MobileHeader: FC = () => {
+  const router = useRouter();
+  const is_mounted = useIsMounted();
+
+  const is_home =
+    categories.find(({ label }) => label == "Shopinger")?.href == router.asPath;
+
+  const is_grocery =
+    categories.find(({ label }) => label == "Grocery")?.href == router.asPath;
+
+  const is_pharmacy =
+    categories.find(({ label }) => label == "Pharmacy")?.href == router.asPath;
+
+  return (
+    <div
+      className={cn(
+        "flex flex-col gap-2 border-none bg-orange-50 px-4 py-3 lg:hidden",
+        is_grocery && "border-b-3 border-green-500",
+        is_pharmacy && "border-b-3 border-blue-500",
+      )}
+    >
+      {/* Delivery & Account */}
+      <div className="flex items-center justify-between">
+        <div className="flex min-w-0 flex-col">
+          <p className="text-lg font-semibold">
+            Delivery in <span className="text-orange-500">10 minutes</span>
+          </p>
+
+          {is_mounted && <LocationTooltip className="flex lg:hidden" />}
+        </div>
+
+        <Link href="/account" aria-label="Account" className="ml-3 shrink-0">
+          <CircleUserIcon className="size-6" aria-hidden />
+        </Link>
+      </div>
+
+      {/* Quick Commerce Categories */}
+      <nav
+        aria-label="Shopinger categories"
+        className="no-scrollbar flex items-center gap-2 overflow-x-auto"
+      >
+        {categories.map(({ label, href, icon }) => {
+          const is_active = href == router.asPath;
+          return (
+            <Link
+              key={label}
+              href={href}
+              className={[
+                "flex h-10 shrink-0 items-center gap-1 rounded-md px-3 text-sm transition-colors",
+                is_active
+                  ? "bg-orange-100 text-orange-500"
+                  : "bg-white text-gray-900 shadow-xs hover:bg-gray-50",
+              ].join(" ")}
+            >
+              {icon}
+
+              {href !== "/" && (
+                <span className={is_active ? "font-semibold" : "font-medium"}>
+                  {label}
+                </span>
+              )}
+            </Link>
+          );
+        })}
+      </nav>
+
+      {/* Search */}
+      <SearchBar />
+    </div>
+  );
+};
+
+export default MobileHeader;
